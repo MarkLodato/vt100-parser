d8c3d56af58393fc77aff2a6657f80f4b9cddbc4
Mark Lodato <lodatom@gmail.com>
2009-05-25 00:27:54 -0400

    HT: tabs should never cause a new line
    If a tab character is input at the end of the line, HT should simply do
    nothing, not append a newline.

--- vt100.py
+++ vt100.py
@@ -441,8 +441,6 @@ class Terminal:
     @command('\x09')        # ^I
     def HT(self, c=None):
         """Horizontal Tab"""
-        if self.col >= self.width:
-            self.NEL()
         while self.col < self.width-1:
             self.col += 1
             if self.tabstops[self.col]:
